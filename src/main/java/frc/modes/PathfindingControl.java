d1f980a5c743d0d9b3891ede3eda322b1d6b136e
Michael Carlstromls <rmc@carlstrom.com>
2019-01-17 19:16:35 -0800

    Added gradient to parhfinding.

--- src/main/java/frc/modes/PathfindingControl.java
+++ src/main/java/frc/modes/PathfindingControl.java
@@ -7,6 +7,8 @@
 
 package frc.modes;
 
+import com.sun.tools.classfile.TypeAnnotation.TargetType;
+
 import frc.fieldmap.geometry.Vector;
 import frc.pathfinding.*;
 import frc.robot.Robot;
@@ -16,8 +18,7 @@ import frc.robot.Robot;
  */
 public class PathfindingControl extends Mode {
 
-    private double x;
-    private double y;
+    private Vector target;
     private Pathfinding pathfinding;
 
     public PathfindingControl() {
@@ -33,28 +34,26 @@ public class PathfindingControl extends Mode {
 
         double xPos = Robot.POS_TRACKER.getX();
         double yPos = Robot.POS_TRACKER.getY();
+        Vector pos = new Vector(xPos, yPos);
         Vector endPos = pathfinding.search(xPos, yPos);
         if (endPos == null) {
             System.out.println("NO PATH FOUND");
             return false;
         }
-        double x = endPos.x;
-        double y = endPos.y;
-        double d = 4 * Math.sqrt((x - xPos) * (x - xPos) + (y - yPos) * (y - yPos));
-        double vx = (x - xPos) / d;
-        double vy = (y - yPos) / d;
-        Robot.SWERVE.drive(vx, vy, 0);
-        if (Math.sqrt((this.x - xPos) * (this.x - xPos) + (this.y - yPos) * (this.y - yPos)) < 0.1) {
+        double d = pos.distanceTo(target);
+        double speed = 0.5 * Math.min((d - 36) / 36.0, 1);
+        Vector velocity = endPos.subtract(pos).multiply(speed / d);
+        if (d < 4) {
             return false;
         }
+        Robot.SWERVE.drive(velocity.x, velocity.y, 0);
         return true;
 
     }
 
     public void setTarget(double x, double y) {
         System.out.println("Setting target to " + x + ", " + y);
-        this.x = x;
-        this.y = y;
+        target = new Vector(x, y);
         pathfinding.setTargetNode(x, y);
 
     }
