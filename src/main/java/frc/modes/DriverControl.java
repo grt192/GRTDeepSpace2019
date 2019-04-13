011a0d56db96e120dd994b05c32861d3e9344976
Audrey Xie <aaxxie13@gmail.com>
2019-04-12 18:01:17 -0700

    get rid of xbox elevator for nixon

--- src/main/java/frc/modes/DriverControl.java
+++ src/main/java/frc/modes/DriverControl.java
@@ -62,32 +62,32 @@ class DriverControl extends Mode {
             Robot.ELEVATOR.setPower(elevatorPower);
         }
 
-        // Mech Driver: goto elevator position
-        switch (Input.MECH_XBOX.getPOV()) {
-        case 0:
-            Robot.ELEVATOR.setPosition(Elevator.ROCKET_TOP);
-            // System.out.println("Rocket Top");
-            break;
-        case 90:
-            Robot.ELEVATOR.setPosition(Elevator.ROCKET_MIDDLE);
-            // System.out.println("Rocket Middle");
-            break;
-        case 180:
-            Robot.ELEVATOR.setPosition(Elevator.ROCKET_BOTTOM);
-            // System.out.println("Rocket Bottom");
-            break;
-        case 270:
-            Robot.ELEVATOR.setPosition(Elevator.CARGO_SHIP);
-            // System.out.println("Cargo Ship");
-            break;
-        }
-        if (Input.MECH_XBOX.getBackButtonPressed()) {
-            Robot.ELEVATOR.setPosition(Elevator.PICKUP);
-        }
+        // // Mech Driver: goto elevator position
+        // switch (Input.MECH_XBOX.getPOV()) {
+        // case 0:
+        // Robot.ELEVATOR.setPosition(Elevator.ROCKET_TOP);
+        // // System.out.println("Rocket Top");
+        // break;
+        // case 90:
+        // Robot.ELEVATOR.setPosition(Elevator.ROCKET_MIDDLE);
+        // // System.out.println("Rocket Middle");
+        // break;
+        // case 180:
+        // Robot.ELEVATOR.setPosition(Elevator.ROCKET_BOTTOM);
+        // // System.out.println("Rocket Bottom");
+        // break;
+        // case 270:
+        // Robot.ELEVATOR.setPosition(Elevator.CARGO_SHIP);
+        // // System.out.println("Cargo Ship");
+        // break;
+        // }
+        // if (Input.MECH_XBOX.getBackButtonPressed()) {
+        // Robot.ELEVATOR.setPosition(Elevator.PICKUP);
+        // }
 
-        if (Input.MECH_XBOX.getStartButtonPressed()) {
-            Robot.BOTTOM_INTAKE.toggle();
-        }
+        // if (Input.MECH_XBOX.getStartButtonPressed()) {
+        // Robot.BOTTOM_INTAKE.toggle();
+        // }
 
         // Experimental stuff
         if (Input.MECH_XBOX.getAButtonPressed()) {
