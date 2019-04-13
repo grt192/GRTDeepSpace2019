eeb2e96fd22c48e585c5fed1e0a2657c36fd2533
Audrey Xie <aaxxie13@gmail.com>
2019-04-12 18:07:19 -0700

    changed back

--- src/main/java/frc/modes/DriverControl.java
+++ src/main/java/frc/modes/DriverControl.java
@@ -62,32 +62,32 @@ class DriverControl extends Mode {
             Robot.ELEVATOR.setPower(elevatorPower);
         }
 
-        // // Mech Driver: goto elevator position
-        // switch (Input.MECH_XBOX.getPOV()) {
-        // case 0:
-        // Robot.ELEVATOR.setPosition(Elevator.ROCKET_TOP);
-        // // System.out.println("Rocket Top");
-        // break;
-        // case 90:
-        // Robot.ELEVATOR.setPosition(Elevator.ROCKET_MIDDLE);
-        // // System.out.println("Rocket Middle");
-        // break;
-        // case 180:
-        // Robot.ELEVATOR.setPosition(Elevator.ROCKET_BOTTOM);
-        // // System.out.println("Rocket Bottom");
-        // break;
-        // case 270:
-        // Robot.ELEVATOR.setPosition(Elevator.CARGO_SHIP);
-        // // System.out.println("Cargo Ship");
-        // break;
-        // }
-        // if (Input.MECH_XBOX.getBackButtonPressed()) {
-        // Robot.ELEVATOR.setPosition(Elevator.PICKUP);
-        // }
+        // Mech Driver: goto elevator position
+        switch (Input.MECH_XBOX.getPOV()) {
+        case 0:
+            Robot.ELEVATOR.setPosition(Elevator.ROCKET_TOP);
+            // System.out.println("Rocket Top");
+            break;
+        case 90:
+            Robot.ELEVATOR.setPosition(Elevator.ROCKET_MIDDLE);
+            // System.out.println("Rocket Middle");
+            break;
+        case 180:
+            Robot.ELEVATOR.setPosition(Elevator.ROCKET_BOTTOM);
+            // System.out.println("Rocket Bottom");
+            break;
+        case 270:
+            Robot.ELEVATOR.setPosition(Elevator.CARGO_SHIP);
+            // System.out.println("Cargo Ship");
+            break;
+        }
+        if (Input.MECH_XBOX.getBackButtonPressed()) {
+            Robot.ELEVATOR.setPosition(Elevator.PICKUP);
+        }
 
-        // if (Input.MECH_XBOX.getStartButtonPressed()) {
-        // Robot.BOTTOM_INTAKE.toggle();
-        // }
+        if (Input.MECH_XBOX.getStartButtonPressed()) {
+            Robot.BOTTOM_INTAKE.toggle();
+        }
 
         // Experimental stuff
         if (Input.MECH_XBOX.getAButtonPressed()) {
@@ -95,12 +95,12 @@ class DriverControl extends Mode {
         } else if (Input.MECH_XBOX.getBButtonPressed()) {
             Sequence.RETRACT_HOOK.start();
         }
-        if (Input.MECH_XBOX.getBumperPressed(Hand.kLeft)) {
-            Robot.CLIMBER.toggleFront();
-        }
-        if (Input.MECH_XBOX.getBumperPressed(Hand.kRight)) {
-            Robot.CLIMBER.toggleBack();
-        }
+        // if (Input.MECH_XBOX.getBumperPressed(Hand.kLeft)) {
+        // Robot.CLIMBER.toggleFront();
+        // }
+        // if (Input.MECH_XBOX.getBumperPressed(Hand.kRight)) {
+        // Robot.CLIMBER.toggleBack();
+        // }
 
     }
 
